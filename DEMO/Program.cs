c8667d5f6512cc31e0a92f79447c146b273d5720
HP <HP@DESKTOP-K4F4GJC>
2025-10-07 10:47:34 +0300

    Video 01

--- DEMO/Program.cs
+++ DEMO/Program.cs
@@ -4,7 +4,40 @@
     {
         static void Main(string[] args)
         {
-          
+            #region Video 01
+
+            /*
+            *************** Architectural Patterns ***************
+            -Web architectural patterns are proven solutions to common design problems in building web applications 
+            - Layered Pattern
+            -يتم تقسيم التطبيق الي 3 طبقات رئيسية 
+                - Presentation Layer
+                - Business Layer
+                - Data Access Layer
+            - Advantages:
+                - Separation of Concerns
+                - Maintainability
+                - Scalability
+                - Testability
+                - Reusability
+
+            -Data Access Layer
+                - Responsible for interacting with the database
+                - Contains methods for CRUD operations (Create, Read, Update, Delete)
+                - Example: UserRepository, ProductRepository
+
+            -Business Layer
+                - Contains the core business logic of the application
+                - Processes data received from the Data Access Layer
+                - Applies business rules and validations
+                - Example: UserService, ProductService
+            -Presentation Layer
+                - Responsible for handling user interactions
+                - Displays data to the user and captures user input
+                - Example: Controllers in MVC, Razor Pages, Blazor Components
+             */
+
+            #endregion
         }
     }
 }
